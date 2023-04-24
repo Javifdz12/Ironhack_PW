147458eb6a308566eb2ca52870da69fec6a5b050
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2023-04-24 18:51:20 +0200

    gg

--- Ejercicio_seriesTV/seriesTV.js
+++ Ejercicio_seriesTV/seriesTV.js
@@ -33,3 +33,19 @@ seriesWatching.push(seriesPending.shift());
 // Imprimir la longitud de ambas matrices
 console.log('Longitud de seriesPending:', seriesPending.length);
 console.log('Longitud de seriesWatching:', seriesWatching.length);
+
+// Bucle que se ejecuta 4 veces
+for (let i = 0; i < 4; i++) {
+    // Eliminar el primer elemento de seriesPending y agregarlo a seriesWatching
+    seriesWatching.push(seriesPending.shift());
+}
+
+// Imprimir la longitud de ambas matrices después del bucle
+console.log('Longitud de seriesPending después del bucle:', seriesPending.length);
+console.log('Longitud de seriesWatching después del bucle:', seriesWatching.length);
+
+// Usar el método .splice() para eliminar todos los elementos de seriesWatching
+seriesWatching.splice(0, seriesWatching.length);
+
+// Imprimir la longitud de seriesWatching después de eliminar los elementos
+console.log('Longitud de seriesWatching después de eliminar los elementos:', seriesWatching.length);
