70e313e2d85975bb4b6e4f448fce01ef6b8ba71f
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2023-05-12 18:44:22 +0200

    gg

--- Primer_proyecto/archivo.js
+++ Primer_proyecto/archivo.js
@@ -2,10 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const formulario = document.getElementById("mi_formulario");
     const nombreInput = document.getElementById("name");
     const mensajeInput = document.getElementById("comment");
-    var boton = document.getElementById("boton");
+    const boton = document.getElementById("boton");
+    const audio = document.getElementById("myAudio");
+    const audio2 = document.getElementById("myAudio2");
 
     boton.addEventListener("mouseover", function() {
-        boton.style.backgroundColor = "green";
+        boton.style.backgroundColor = "red";
+        if (validarFormulario2()) {
+            boton.style.backgroundColor = "green";
+        }
     });
 
     boton.addEventListener("mouseout", function() {
@@ -29,26 +34,24 @@ document.addEventListener("DOMContentLoaded", function() {
         return true;
     }
 
-    formulario.addEventListener("submit", function (event) {
+    function validarFormulario2() {
+        if (nombreInput.value.trim() === "") {
+            return false;
+        }
+
+        if (mensajeInput.value.trim() === "") {
+            return false;
+        }
+
+        return true;
+    }
+
+    boton.addEventListener("click", function (event) {
         event.preventDefault();
 
         if (validarFormulario()) {
-            const solicitud = new XMLHttpRequest();
-            solicitud.onreadystatechange = function() {
-                if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-                // Si la solicitud ha sido exitosa, mostrar el mensaje de confirmación
-                mensaje.innerHTML = "Tu comentario ha sido guardado correctamente.";
-                formulario.reset(); // Reiniciar el formulario
-                } else if (this.readyState === XMLHttpRequest.DONE) {
-                // Si la solicitud ha fallado, mostrar un mensaje de error
-                mensaje.innerHTML = "Ha ocurrido un error al guardar tu comentario.";
-                }
-            };
-            solicitud.open("POST", "guardar.php", true);
-            solicitud.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            solicitud.send("nombre=" + nombreInput + "&comentario=" + mensajeInput);
-            // Recargar la página
-            location.reload();
+            audio.play();
+            formulario.reset();
         }
     });
 });
